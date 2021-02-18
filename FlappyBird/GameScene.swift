59f518b15a437eca635b91a6779061cf28f23ba2
Masa.Yashi <myaassahfiummai.10.11@gmail.com>
2021-02-19 00:36:44 +0900

    背景色を設定

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -10,6 +10,13 @@ import SpriteKit
 
 class GameScene: SKScene {
 
+    //SKView上にシーンが表示された時に呼ばれるメソッド
+    override func didMove(to view: SKView) {
+        //背景色を設定
+        backgroundColor = UIColor(red: 0.15, green: 0.75, blue: 0.90, alpha: 1)
+    }
+    
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
