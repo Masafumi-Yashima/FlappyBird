3427aeeed56b3e456e64781834cee5171bd12218
Masa.Yashi <myaassahfiummai.10.11@gmail.com>
2021-02-19 00:20:14 +0900

    画像の追加 SKSceneクラスを継承したクラスを作るまで完了

---
+++ FlappyBird/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  FlappyBird
+//
+//  Created by YashimaMasafumi on 2021/02/18.
+//
+
+import UIKit
+import SpriteKit
+
+class GameScene: SKScene {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
