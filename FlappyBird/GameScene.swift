ec9cb250fe0095092b00c518e39928bb2bf4a61b
Masa.Yashi <myaassahfiummai.10.11@gmail.com>
2021-02-20 13:13:57 +0900

    スプライをを表示する

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -14,6 +14,22 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         //背景色を設定
         backgroundColor = UIColor(red: 0.15, green: 0.75, blue: 0.90, alpha: 1)
+        
+        //地面の画像を読み込む
+        let groundTexture = SKTexture(imageNamed: "ground")
+        groundTexture.filteringMode = SKTextureFilteringMode.nearest
+        
+        //テクスチャを指定してスプライトを作成する
+        let groundSprite = SKSpriteNode(texture: groundTexture)
+        
+        //スプライトの表示する位置を指定する
+        groundSprite.position = CGPoint(
+            x: groundTexture.size().width/2,
+            y: groundTexture.size().height/2
+        )
+        
+        //シーンにスプライトを表示する
+        addChild(groundSprite)
     }
     
     
